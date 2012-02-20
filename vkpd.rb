88202ee02d71af7ddbea4ed2393eaee309c24d8c
Ales Guzik <ales.guzik@gmail.com>
2012-02-20 22:51:59 +0300

    some fixes, some improvements

--- vkpd.rb
+++ vkpd.rb
@@ -10,26 +10,33 @@ params = {}
 action_before = 'mpc clear'
 action_after  = 'mpc play'
 
+config = YAML.load File.read("#{ENV['HOME']}/.config/vkpd.yaml")
+params["access_token"]=config["access_token"]
+
 while ARGV.size > 0
   current = ARGV.shift
   case current
   when '-h','--help'
     puts man=<<EOF
-manual here
+Fork me, write manual and make pull-request
 EOF
     exit 0
-  when '-c', '--count', /--count=\d+/
+  when '-c', '--count', /^--count=\d+$/
     value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
     params["count"]  = value
-  when '-o', '--offset', /--offset=\d+/
+  when '-o', '--offset', /^--offset=\d+$/
     value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
     params["offset"] = value
-  when '-s', '--sort', /--sort=\d+/
+  when '-s', '--sort', /^--sort=\d+$/
     value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
     params["sort"] = value
   when 'user'
     method = 'audio.get'
-    params['uid'] = ARGV.shift
+    if !ARGV.empty? and ARGV.first.match(/^\d+$/)
+      params['uid'] = ARGV.shift
+    else
+      params['uid'] = config["user_id"]
+    end
   when 'group'
     method = 'audio.get'
     params['gid'] = ARGV.shift
@@ -50,25 +57,20 @@ def hash_to_params(hash)
   hash.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join("&")
 end
 
-if File.exist? "#{ENV['HOME']}/.config/vkpd.yaml"
-  config = YAML.load File.read("#{ENV['HOME']}/.config/vkpd.yaml")
-#  params["uid"]=config["user_id"]
-  params["access_token"]=config["access_token"]
-
-  connection=Net::HTTP.new("api.vkontakte.ru",443)
-  connection.use_ssl=true
-  p data=connection.get("/method/#{method}?#{hash_to_params(params)}").body
-  response = JSON.parse(data)["response"][1..-1]
-  system action_before
-  # playlist_file = File.open("vkpd.m3u","w")
-  # playlist_file.puts "#EXTM3U\n"
-  response.each do |song|
-    #  playlist_file.puts "#EXTINF:#{song["duration"]}, #{song["artist"]} - #{song["title"]}"
-    #  playlist_file.puts "#{song["url"]}\n"
-    system "mpc add #{song["url"]}"
-  end
-  # playlist_file.close
-  system action_after
-else
+unless File.exist? "#{ENV['HOME']}/.config/vkpd.yaml"
   puts 'Please authenticate. Start vkpd-auth.rb and point your browser to http://localhost.local:4567/'
+  exit 1
 end
+
+connection=Net::HTTP.new("api.vkontakte.ru",443)
+connection.use_ssl=true
+data=connection.get("/method/#{method}?#{hash_to_params(params)}").body
+response = JSON.parse(data)["response"]
+if method.match /search/
+  response.shift
+end
+system action_before
+response.each do |song|
+  system "mpc add #{song["url"]}"
+end
+system action_after
