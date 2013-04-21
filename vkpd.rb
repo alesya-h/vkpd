84e8f3271960e1853a420b7d41b5565744c4b6fa
Ales Guzik <me@aguzik.net>
2013-04-21 21:34:52 +0300

    WIP

--- vkpd.rb
+++ vkpd.rb
@@ -4,90 +4,125 @@ require "net/https"
 require "cgi"
 require "yaml"
 require "json"
+require "optparse"
 
-method = "audio.search"
 params = {}
-action_before = 'mpc clear'
 action_after  = 'mpc play'
 params["auto_complete"] = '1'
 
-if ARGV.empty?
-  ARGV.push "-h"
-end
+module VKPD; end
+VKPD.instance_eval do
 
-
-def config
-  unless File.exist? "#{ENV['HOME']}/.config/vkpd.yaml"
-    puts 'Please authenticate. Start vkpd-auth.rb and point your browser to http://localhost.localdomain:4567/'
-    exit 1
+  def hash_to_params(hash)
+    hash.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join("&")
   end
-  @config ||= YAML.load File.read("#{ENV['HOME']}/.config/vkpd.yaml")
-end
-
-while ARGV.size > 0
-current = ARGV.shift
-  case current
-  when '-h','--help'
-    filename = `dirname #{`readlink -f #{$0}`}`.chomp+"/README"
-    puts File.read(filename)
-    exit 0
-  when '-d', '--debug'
-    $debug  = true
-  when '-c', '--count', /^--count=\d+$/
-    value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
-    params["count"]  = value
-  when '-o', '--offset', /^--offset=\d+$/
-    value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
-    params["offset"] = value
-  when '-s', '--sort', /^--sort=\d+$/
-    value = current.include?("=") ? current.match(/=(.*)/)[1] : ARGV.shift
-    params["sort"] = value
-  when 'user'
-    method = 'audio.get'
-    if !ARGV.empty? and ARGV.first.match(/^\d+$/)
-      params['uid'] = ARGV.shift
-    else
-      params['uid'] = config["user_id"]
+  def query_vk method, params
+    params["access_token"]=config["access_token"]
+    connection=Net::HTTP.new("api.vk.com",443)
+    connection.use_ssl=true
+    data=connection.get("/method/#{method}?#{hash_to_params(params)}").body
+    response = JSON.parse(data)["response"]
+    if method.match /search/
+      response.shift
     end
-  when 'group'
-    method = 'audio.get'
-    params['gid'] = ARGV.shift
-  when 'add'
-    action_before = ''
-    action_after = ''
-  when '-nf','--no-fix', '--exact'
-    params["auto_complete"] = '0'
-  when 'play'
-    # do nothing
-  else
-    params["q"]=ARGV.unshift(current).join(" ")
-    ARGV.clear
+    response.map{ |song| song["url"] }
   end
-end
-
-def run(*args)
-  if $debug
-    puts(*args)
-  else
-    system(*args)
+  def config
+    unless File.exist? "#{ENV['HOME']}/.config/vkpd.yaml"
+      puts 'Please authenticate. Start vkpd-auth.rb and point your browser to http://localhost.localdomain:4567/'
+      exit 1
+    end
+    $config ||= YAML.load File.read("#{ENV['HOME']}/.config/vkpd.yaml")
   end
+
+  def show_help
+    filename = "#{__dir__}/README"
+    puts File.read(filename)
+  end
+  def mpd_add song_url
+    execute "mpc add #{song_url}"
+  end
+  def mpd_clear
+    execute "mpc clear"
+  end
+  def add_songs songs
+    songs.each{ |song| mpd_add song }
+  end
+  def replace_songs songs
+    mpd_clear
+    add_songs songs
+  end
+  def mpd_play
+    execute "mpc clear"
+  end
+    
+  def run options
+    # case current
+    # when '-h','--help'
+    #   show_help
+    #   return
+    # when '-d', '--debug'
+    #   $debug = true
+    # when '-c', '--count', /^--count=\d+$/
+    #   value = current.include?("=") ? current.match(/=(.*)/)[1] : args.shift
+    #   params["count"]  = value
+    # when '-o', '--offset', /^--offset=\d+$/
+    #   value = current.include?("=") ? current.match(/=(.*)/)[1] : args.shift
+    #   params["offset"] = value
+    # when '-s', '--sort', /^--sort=\d+$/
+    #   value = current.include?("=") ? current.match(/=(.*)/)[1] : args.shift
+    #   params["sort"] = value
+    # when 'user'
+    #   method = 'audio.get'
+    #   if !args.empty? and args.first.match(/^\d+$/)
+    #     params['uid'] = args.shift
+    #   else
+    #     params['uid'] = config["user_id"]
+    #   end
+    # when 'group'
+    #   method = 'audio.get'
+    #   params['gid'] = args.shift
+    # when 'add'
+    #   action_before = ''
+    #   action_after = ''
+    # when '-nf','--no-fix', '--exact'
+    #   params["auto_complete"] = '0'
+    # when 'play'
+    #   # do nothing
+    # else
+    #   params["q"]=args.unshift(current).join(" ")
+    #   args.clear
+    # end
+  end
+
+  def execute(*args)
+    if $debug
+      puts(*args)
+    else
+      system(*args)
+    end
+  end
+
 end
 
-def hash_to_params(hash)
-  hash.map{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join("&")
-end
+o = {sort: 0, verbose: false, offset: 0}
 
-params["access_token"]=config["access_token"]
+OptionParser.new do |opts|
+  opts.banner = "Usage: vkpd [options] [search request]"
+  opts.on("-v", "--verbose",
+          "Run verbosely"){            |arg| o[:verbose] = arg }
+  opts.on("-u", "--vk-user name",
+          "Play vk user's songs"){     |arg| o[:vk_user] = arg }
+  opts.on("-l", "--lastfm-user name",
+          "Play lastfm user's songs"){ |arg| o[:lastfm_user] = arg }
+  opts.on("-c", "--count n",
+          "Add no more than n songs"){ |arg| o[:limit] = arg }
+  opts.on("-o", "--offset n",
+          "Add no more than n songs"){ |arg| o[:offset] = arg }
+  opts.on("-s", "--sort",
+          "Results sorting order",
+          "(0 - popularity, 1 - length, 2 - upload date)"){ |arg| o[:sort] = arg }
+  opts.on_tail("-h", "--help", "Show this message"){ puts opts; exit }
+end.parse!
 
-connection=Net::HTTP.new("api.vk.com",443)
-connection.use_ssl=true
-data=connection.get("/method/#{method}?#{hash_to_params(params)}").body
-response = JSON.parse(data)["response"]
-if method.match /search/
-  response.shift
-end
-run action_before
-response.each do |song|
-  run "mpc add #{song["url"]}"
-end
-run action_after
+VKPD.run(o)
